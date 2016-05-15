2f67f548711a78b1e23d15d2989f717300fe88e2
f00dl3 <f00dl3@users.noreply.github.com>
2016-05-15 08:59:03 -0500

    Update MakeMP4.sh
    Joins MP4s at given time of day. Much faster than raw images. Slow part is ffprobe to get frame count. This can be removed if not needed.

--- BootBundle.zip/MakeMP4.sh
+++ BootBundle.zip/MakeMP4.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-Email="YourEmail@server.com"
+EMail=
+SourceFolder="/var/www/Get/Cams/Archive"
+UnpackFolder="/dev/shm/mp4tmp"
+getYesterday=$(date -d "yesterday 12:00" +'%y%m%d')
 
-mkdir -p /dev/shm/mp4tmp
-unzip -j /var/www/Get/Cams/Archive/"*.zip" -d /dev/shm/mp4tmp/
-rm /var/www/Get/Cams/Archive/*.zip
-CamImgQty=$(ls -l /dev/shm/mp4tmp/*.jpeg | wc -l)
-CamSizeSum=$(du -c /dev/shm/mp4tmp/*.jpeg | sed -n "\$s/\\t.*//p")
-bash /dev/shm/Sequence.sh /dev/shm/mp4tmp/ jpeg
-avconv -framerate 24 -i /dev/shm/mp4tmp/%05d.jpeg -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" /var/www/Get/Cams/MP4/$(date -d "yesterday 12:00" +'%y%m%d').mp4 2> /dev/shm/mp4tmp/avconv.log
-CamMP4Size=$(du -c /var/www/Get/Cams/MP4/$(date -d "yesterday 12:00" +'%y%m%d').mp4 | sed -n "\$s/\\t.*//p")
+mkdir -p $UnpackFolder
+#zip -9rv /home/User/Desktop/CamBack-$(date -d "yesterday 12:00" +'%y%m%d').zip $SourceFolder/*
+mv $SourceFolder/* $UnpackFolder
+bash /dev/shm/Sequence.sh $UnpackFolder/ mp4
+for filename in $UnpackFolder/*.mp4; do
+	echo "file '$filename'" >> $UnpackFolder/Listing.txt
+done
+ffmpeg -threads 8 -f concat -i $UnpackFolder/Listing.txt -c copy /var/www/Get/Cams/MP4/$getYesterday.mp4 2> /var/www/Get/Cams/MakeMP4_Last.log
+CamImgQty=$(ffprobe -v error -count_frames -select_streams v:0 -show_entries stream=nb_read_frames -of default=nokey=1:noprint_wrappers=1 /var/www/Get/Cams/MP4/$getYesterday.mp4)
+CamMP4Size=$(du -c /var/www/Get/Cams/MP4/$getYesterday.mp4 | sed -n "\$s/\\t.*//p")
 (ls /var/www/Get/Cams/MP4/*.mp4 -t | head -n 60; ls /var/www/Get/Cams/MP4/*.mp4)|sort|uniq -u|xargs rm
 chown -R www-data /var/www/Get/Cams/MP4/
 mysql Core << EOF
-insert into Log_CamsMP4 (Date,ImgCount,TotSize,MP4Size) values (CURDATE()-1,'$CamImgQty','$CamSizeSum','$CamMP4Size');
+insert into Log_CamsMP4 (Date,ImgCount,MP4Size) values (CURDATE()-1,'$CamImgQty','$CamMP4Size');
 EOF
-MP4Listing=$(ls -s /var/www/Get/Cams/MP4)
-echo "AVConv Log Attached! \n $MP4Listing" | mail -s "AVConv nightly logfile." -a /dev/shm/mp4tmp/avconv.log $Email
-rm -fr /dev/shm/mp4tmp
+rm -fr $UnpackFolder
