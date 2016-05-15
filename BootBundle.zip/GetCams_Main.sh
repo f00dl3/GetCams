0016fb8aa7b55a99323cc7e9a65b712bb9106a10
f00dl3 <f00dl3@users.noreply.github.com>
2016-05-15 08:57:25 -0500

    Update GetCams_Main.sh
    5/15/16 modifications. Fixed issues with email/text messaging functions. Added redundant catcher if wired cam is unplugged, defaults to wireless mode. Stored passwords in variables instead of in the command to connect to cams.

--- BootBundle.zip/GetCams_Main.sh
+++ BootBundle.zip/GetCams_Main.sh
@@ -14,14 +14,17 @@ UACam1T=$(cat /dev/shm/GetCams/OffCam1T.txt)
 UACam2T=$(cat /dev/shm/GetCams/OffCam2T.txt)
 UACam3T=$(cat /dev/shm/GetCams/OffCam3T.txt)
 
+ipCamUser=
+ipCamPass=
+mTrigger=250
+
 (
-curl --max-time 3 "http://IPCam1Address/cgi-bin/CGIProxy.fcgi?cmd=snapPicture2&usr=IPCamUser&pwd=IPCamPass" > /dev/shm/GetCams/Xwebc3-temp.jpeg &
-curl --max-time 3 "http://IPCam2Address/cgi-bin/CGIProxy.fcgi?cmd=snapPicture2&usr=IPCamUser&pwd=IPCamPass" > /dev/shm/GetCams/Xwebc2-temp.jpeg
+curl --max-time 3 "http://?:88/cgi-bin/CGIProxy.fcgi?cmd=snapPicture2&usr=$ipCamUser&pwd=$ipCamPass" > /dev/shm/GetCams/Xwebc3-temp.jpeg &
+curl --max-time 3 "http://?:88/cgi-bin/CGIProxy.fcgi?cmd=snapPicture2&usr=$ipCamUser&pwd=$ipCamPass" > /dev/shm/GetCams/Xwebc2-temp.jpeg
 )
 
-if [ -e "/dev/video"* ]; then
-	USBCamRef=$(ls /dev/video*)
-	timeout --kill-after=15 15 avconv -f video4linux2 -s 1920x1080 -i $USBCamRef -ss 00:00:01.0 -frames 1 /dev/shm/GetCams/Xwebc1-temp.jpeg
+if [ ! -s /dev/shm/GetCams/Xwebc3-temp.jpeg ]; then
+	curl --max-time 3 "http://?:88/cgi-bin/CGIProxy.fcgi?cmd=snapPicture2&usr=$ipCamUser&pwd=$ipCamPass" > /dev/shm/GetCams/Xwebc3-temp.jpeg
 fi
 
 MainLabel="$(date +'%Y-%m-%d %H:%M:%S') -- OJC ${LOJCTF}F -- IN ${LCaseTF}F -- CPU ${LCPUTF}F -- ${LUStatus}"
@@ -29,7 +32,7 @@ MainLabel="$(date +'%Y-%m-%d %H:%M:%S') -- OJC ${LOJCTF}F -- IN ${LCaseTF}F -- C
 if [ ! -f /dev/shm/GetCams/Xwebc1-temp.jpeg ]; then
 	UACam1=$((UACam1 + 1))
 	UACam1T=$((UACam1T + 1))
-	if [ $UACam1 -eq 100 ]; then
+	if [ $UACam1 -eq $mTrigger ]; then
 		echo "Camera 1 offline more than 100 cycles!" | mail -s "Camera 1 offline!" $SendTo
 	fi
 	convert -size 1920x1080 -gravity center -annotate 0 "Cam1 temporarily unavailable!\n Cycles: ${UACam1}" -pointsize 48 -fill Yellow xc:navy /dev/shm/GetCams/Xwebc1-temp.jpeg
@@ -42,7 +45,7 @@ fi
 if [ ! -s /dev/shm/GetCams/Xwebc2-temp.jpeg ]; then
 	UACam2=$((UACam2 + 1))
 	UACam2T=$((UACam2T + 1))
-	if [ $UACam3 -eq 100 ]; then
+	if [ $UACam3 -eq $mTrigger ]; then
 		echo "Camera 2 offline more than 100 cycles!" | mail -s "Camera 2 offline!" $SendTo
 	fi
 	convert -size 954x540 -gravity center -annotate 0 "Cam2 temporarily unavailable!\n Cycles:  ${UACam2}" -pointsize 48 -fill Yellow xc:navy /dev/shm/GetCams/Xwebc2-temp.jpeg
@@ -54,8 +57,8 @@ fi
 
 if [ ! -s /dev/shm/GetCams/Xwebc3-temp.jpeg ]; then
 	UACam3=$((UACam3 + 1))
-	UACam3T=$((UACam1T + 1))
-	if [ $UACam3 -eq 100 ]; then
+	UACam3T=$((UACam3T + 1))
+	if [ $UACam3 -eq $mTrigger ]; then
 		echo "Camera 3 offline more than 100 cycles!" | mail -s "Camera 3 offline!" $SendTo
 	fi
 	convert -size 954x540 -gravity center -annotate 0 "Cam3 temporarily unavailable!\n Cycles: ${UACam3}" -pointsize 48 -fill Yellow xc:navy /dev/shm/GetCams/Xwebc3-temp.jpeg
